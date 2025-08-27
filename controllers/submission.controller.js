228edf57bfd5475f81d9b193c01020fe495090e2
Wolfsmirks <Jacob.Guest.0@gmail.com>
2025-08-27 22:41:14 +0100

    Update submission controller

--- controllers/submission.controller.js
+++ controllers/submission.controller.js
@@ -1,20 +1,33 @@
 import { sendSubmission } from "../models/submission.model.js";
-
-const code = `
-function add(a, b) {
-  return a + b;
-}
-`.trim();
-
-const test = `
-add(1, 2) === 3 ? "PASS" : "FAIL"
-`.trim();
+import { testsQuery } from "../queries/tests.query.js";
+import { isPrimitive } from "../utils.js";
 
 export async function postSubmission(req, res) {
+  const { code, signature, kata_id } = req.body;
+
+  // const { input, expected } = testsQuery(kata_id);
+  const input = [1, 2];
+  const expected = 3;
+
+  let assertions = "";
+
+  if (isPrimitive(expected)) {
+    assertions += `assert.strictEqual(${signature}(${input[0]}, ${input[1]}), ${expected})`;
+  } else {
+    assertions += `assert.deepEqual(${signature}(${input[0]}, ${input[1]}), ${expected})`;
+  }
+
   const sourceCode = `
+  import assert from "assert";
   ${code}
-  console.log(${test});
-  `;
+  try {
+    ${assertions};
+    console.log("PASS");
+  } catch {
+    console.log("FAIL");
+  };
+  `.trim();
+
   const result = await sendSubmission(sourceCode);
   const resultJson = JSON.stringify({ output: result });
   res.status(201).send(resultJson);

ddd7c6de9bc210be4a64a357ad874d9f2f725b8b
melissa <melissa.pagejones@googlemail.com>
2025-08-28 15:57:59 +0100

    Testing completed for seeding

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -9,11 +9,8 @@ import {
   createNotes,
   createKataTags,
 } from "./manage-tables.js";
-import { formatData } from "../../utils.js";
 
-export async function seed({ kataData, testData, hintData, tagData }) {
-  console.log(kataData);
-
+export async function seed(kataData, testData, hintData, tagData, noteData) {
   try {
     await dropTables("kata_tags", "tests", "hints", "notes", "tags", "katas");
 
@@ -24,11 +21,11 @@ export async function seed({ kataData, testData, hintData, tagData }) {
     await createNotes();
     await createKataTags();
 
-    await insertData("katas", formatData(kataData));
-    await insertData("tests", formatData(testData));
-    await insertData("hints", formatData(hintData));
-    await insertData("tags", formatData(tagData));
-    await insertData("notes", formatData(noteData));
+    await insertData("katas", kataData);
+    await insertData("tests", testData);
+    await insertData("hints", hintData);
+    await insertData("tags", tagData);
+    await insertData("notes", noteData);
   } catch (err) {
     return err;
   }

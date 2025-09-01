c987db6fe0b324b3f14b2f04f0ff40a31f6aac06
Wolfsmirks <Jacob.Guest.0@gmail.com>
2025-09-01 11:56:15 +0100

    Update tests.query.js to use parameterisation

--- queries/tests.query.js
+++ queries/tests.query.js
@@ -3,7 +3,8 @@ import db from "../db/connection.js";
 export async function testsQuery(id) {
   try {
     const kataTests = {
-      text: `SELECT signature, input, expected FROM tests WHERE kata_id = ${id}`,
+      text: `SELECT signature, input, expected FROM tests WHERE kata_id = $1`,
+      value: [id],
       rowMode: "object",
     };
     return await db.query(kataTests);

59dc5aa8f9c81353b3192ec9efbb325deddc612e
Wolfsmirks <Jacob.Guest.0@gmail.com>
2025-09-01 14:45:32 +0100

    Fix error-handler export bug

--- error-handlers/index.js
+++ error-handlers/index.js
@@ -1,3 +1,3 @@
-export { default as handleBadRequest } from "./handleBadRequest";
-export { default as handleCustomError } from "./handleCustomError";
-export { default as handleServerError } from "./handleServerError";
+export { default as handleBadRequest } from "./handleBadRequest.js";
+export { default as handleCustomError } from "./handleCustomError.js";
+export { default as handleServerError } from "./handleServerError.js";

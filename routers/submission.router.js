200663dff9d54944029922369b12fefa10a72549
Wolfsmirks <Jacob.Guest.0@gmail.com>
2025-09-01 14:48:07 +0100

    Fix submission router export bug

--- routers/submission.router.js
+++ routers/submission.router.js
@@ -1,5 +1,5 @@
 import express from "express";
-import postSubmission from "../controllers/submission.controller";
+import postSubmission from "../controllers/submission.controller.js";
 
 const submissionRouter = express.Router();
 
